c147c7a9d7dce8383596ac951930318e32bcb460
Brayan Yepes <abrayan.claye@gmail.com>
2024-08-02 15:11:38 -0500

    feat: agg function to open envelop and generate laminas

--- src/components/atoms/Sobre/Sobre.tsx
+++ src/components/atoms/Sobre/Sobre.tsx
@@ -1,8 +1,15 @@
+import { generarLaminas } from "../../../api/generarLaminas";
+import useStore from "../../../store/store"
 import { SobreProps } from "./types"
 
 export const Sobre = ({ name }: SobreProps) => {
+    const { aggLaminas } = useStore();
+    const openSobre = async() => {
+        const laminasGeneradas = await generarLaminas();
+        aggLaminas(laminasGeneradas);
+    }
     return (
-        <div className="w-full h-full p-4 text-center hover:bg-gray-900 transition-all cursor-pointer bg-gray-800 text-white rounded">
+        <div onClick={openSobre} className="w-full h-full p-4 text-center hover:bg-gray-900 transition-all cursor-pointer bg-gray-800 text-white rounded">
             { name }
         </div>
     )
