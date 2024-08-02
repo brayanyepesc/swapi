b9d3208cd69d297acd64f6334c9dec8125f1c149
Brayan Yepes <abrayan.claye@gmail.com>
2024-08-02 15:08:47 -0500

    feat: add a button to reset laminas store and estrcuture laminas components

--- src/components/molecules/LaminasList/LaminasList.tsx
+++ src/components/molecules/LaminasList/LaminasList.tsx
@@ -1,18 +1,12 @@
-import useStore from "../../../store/store"
 import { LaminasListItem } from "../LaminasListItem/LaminasListItem"
 
-export const LaminasList = () => {
-    const { laminas } = useStore();
-    const laminasGeneradas = [
-        ...(laminas.peliculas ?? []),
-        ...(laminas.personajes ?? []),
-        ...(laminas.naves ?? [])
-    ];
+export const LaminasList = ({ laminasGeneradas, hasLaminas }: { laminasGeneradas: any[], hasLaminas: boolean }) => {
+    
     return (
         <>
             <ul className="h-full w-full grid grid-cols-2 gap-2">
                 {
-                    laminasGeneradas.length > 0 ? laminasGeneradas.map((_, index) => (
+                    hasLaminas ? laminasGeneradas.map((_, index) => (
                         <li key={index}>
                             <LaminasListItem />
                         </li>
--- src/components/organisms/LaminasContainer/LaminasContainer.tsx
+++ src/components/organisms/LaminasContainer/LaminasContainer.tsx
@@ -1,13 +1,24 @@
+import useStore from "../../../store/store";
 import { LaminasList } from "../../molecules"
 
 export const LaminasContainer = () => {
+    const { laminas } = useStore();
+    const laminasGeneradas = [
+        ...(laminas.peliculas ?? []),
+        ...(laminas.personajes ?? []),
+        ...(laminas.naves ?? [])
+    ];
+    const hasLaminas: boolean = laminasGeneradas.length > 0;
     return (
         <div className="flex flex-col justify-center items-center p-5 bg-gray-100">
             <div className="flex flex-col justify-center items-center my-4">
                 <h2 className="text-gray-800 text-xl font-bold">Láminas generadas</h2>
                 <p className="my-2 text-sm">Acá estarán disponibles las lámitas generadas que podrás añadir a tu álbum de colección</p>
             </div>
-            <LaminasList />
+            <LaminasList laminasGeneradas={laminasGeneradas} hasLaminas={hasLaminas}/>
+            {
+                hasLaminas && (<button className="bg-blue-500 hover:bg-blue-700 text-white rounded p-1 flex self-end">Borrar láminas</button>)
+            }
         </div>
     )
 }
