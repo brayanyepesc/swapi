7d0d9e4eadc395834dc4478909152a101a609c37
Brayan Yepes <abrayan.claye@gmail.com>
2024-08-02 09:17:36 -0500

    feat: add laminas page with available envelops to click

--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -5,7 +5,7 @@ export const Header = ({ headerTitle, headerDescription }: HeaderProps) => {
     return (
         <header className="w-full p-5 shadow-lg rounded">
             <h1 className="text-xl text-center font-bold text-gray-500 mb-2">{headerTitle}</h1>
-            <p className="text-sm">{headerDescription ? headerDescription : extensiveTextMock}</p>
+            <p className="text-sm text-center">{headerDescription ? headerDescription : extensiveTextMock}</p>
         </header>
     )
 }
--- src/pages/Laminas.tsx
+++ src/pages/Laminas.tsx
@@ -1,5 +1,35 @@
-export default function Laminas (){
+import { Header } from "../components/molecules";
+
+export default function Laminas() {
     return (
-        <main>Laminas</main>
+        <main className="p-5 w-full max-h-screen">
+            <Header
+                headerTitle="Obtener láminas"
+                headerDescription="Aquí puedes obtener láminas de la api de starwars"
+            />
+            <div className="grid grid-cols-2 gap-5 border-separate divide-x-2 mt-2 p-5 rounded shadow-lg w-full h-full">
+                <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center my-4">
+                        <h2 className="text-gray-800 text-xl font-bold">Sobres disponibles</h2>
+                        <p className="my-2 text-sm">Selecciona un sobre para generar las láminas</p>
+                    </div>
+                    <ul className="h-full w-full grid grid-cols-2 gap-2">
+                        <li className="w-full h-full p-4 text-center hover:bg-gray-900 transition-all cursor-pointer bg-gray-800 text-white rounded">
+                            Sobre
+                        </li>
+                        <li className="w-full h-full p-4 text-center hover:bg-gray-900 transition-all cursor-pointer bg-gray-800 text-white rounded">
+                            Sobre
+                        </li>
+                        <li className="w-full h-full p-4 text-center hover:bg-gray-900 transition-all cursor-pointer bg-gray-800 text-white rounded">
+                            Sobre
+                        </li>
+                        <li className="w-full h-full p-4 text-center hover:bg-gray-900 transition-all cursor-pointer bg-gray-800 text-white rounded">
+                            Sobre
+                        </li>
+                    </ul>
+                </div>
+                <div className="flex justify-center items-center">Hasta el momento no tienes láminas generadas</div>
+            </div>
+        </main>
     )
 }
