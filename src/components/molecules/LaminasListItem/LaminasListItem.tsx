cd2bbc838451cb2965cc5f835c67d2a61f4b9684
Brayan Yepes <abrayan.claye@gmail.com>
2024-08-02 15:54:47 -0500

    feat: add action btns in laminaslistitem

--- src/components/molecules/LaminasListItem/LaminasListItem.tsx
+++ src/components/molecules/LaminasListItem/LaminasListItem.tsx
@@ -4,7 +4,7 @@ import { LaminasListItemProps } from "./types"
 export const LaminasListItem = ({ lamina }: LaminasListItemProps) => {
   const infoLamina = useInfoLamina({ lamina });
   return (
-    <div className="hover:bg-gray-200 w-full space-y-1 h-full text-center border p-2 transition-all cursor-pointer text-white rounded">
+    <div className="w-full space-y-1 h-full text-center border p-2 transition-all text-white rounded">
       {
         Object.entries(infoLamina).map(([key, value], index) => (
           <div key={index} className="rounded text-gray-500 shadow-sm p-1 flex justify-between items-center">
@@ -13,6 +13,10 @@ export const LaminasListItem = ({ lamina }: LaminasListItemProps) => {
           </div>
         ))
       }
+      <div className="w-full flex gap-2">
+        <button className="w-full p-1 bg-gray-500 hover:bg-gray-700 rounded">Ver</button>
+        <button className="w-full p-1 bg-blue-500 hover:bg-blue-700 rounded">Agregar</button>
+      </div>
     </div>
   )
 }
