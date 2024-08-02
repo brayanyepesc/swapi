c0496a670e8bdd0f2fd4f7ded44553511f40ebab
Brayan Yepes <abrayan.claye@gmail.com>
2024-08-02 09:01:35 -0500

    ref: add a text mock in headers components

--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -1,11 +1,11 @@
+import { extensiveTextMock } from "../../../utils/constantes"
 import { HeaderProps } from "./types"
 
 export const Header = ({ headerTitle, headerDescription }: HeaderProps) => {
-    const defaultDescription = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia cum ratione perspiciatis fuga illum expedita distinctio nulla reiciendis laudantium maiores porro quaerat, similique eum eligendi magnam commodi reprehenderit repudiandae dolore! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi nulla ut doloremque iste quae soluta. Fugiat, neque optio. Ipsa dolores voluptate quaerat quisquam. Est voluptas placeat, praesentium illum dolorem asperiores?"
     return (
         <header className="w-full p-5 shadow-lg rounded">
             <h1 className="text-xl text-center font-bold text-gray-500 mb-2">{headerTitle}</h1>
-            <p className="text-sm">{headerDescription ? headerDescription : defaultDescription}</p>
+            <p className="text-sm">{headerDescription ? headerDescription : extensiveTextMock}</p>
         </header>
     )
 }
--- src/components/organisms/AlbumHeader/AlbumHeader.tsx
+++ src/components/organisms/AlbumHeader/AlbumHeader.tsx
@@ -1,7 +1,7 @@
+import { extensiveTextMock } from "../../../utils/constantes"
 import { AlbumHeaderProps } from "./types"
 
 export const AlbumHeader = ({ imgUrl, sectionTitle, description, btnTitle }: AlbumHeaderProps) => {
-    const defaultDescription = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur, voluptatum corrupti hic temporibus sapiente distinctio omnis vel fugit fugiat quibusdam, suscipit aspernatur perferendis beatae, libero dicta cumque quas eos ducimus."
     const defaultBtnTitle = "Obtener más láminas"
     return (
         <section className="w-full h-96 p-4 bg-gray-200 place-content-center shadow-xl">
@@ -11,7 +11,7 @@ export const AlbumHeader = ({ imgUrl, sectionTitle, description, btnTitle }: Alb
                 </div>
                 <div className="flex flex-col justify-center space-y-8 p-4">
                     <h1 className="text-4xl font-bold text-blue-500">{sectionTitle}</h1>
-                    <p className="text-gray-500">{description ? description : defaultDescription}</p>
+                    <p className="text-gray-500">{description ? description : extensiveTextMock}</p>
                     <div className="w-full">
                         <button className="p-2 rounded bg-blue-500 text-white hover:bg-blue-700 transition-all">{btnTitle ? btnTitle : defaultBtnTitle}</button>
                     </div>
