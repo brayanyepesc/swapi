37ab4f0e19970c41e7797e810cf48cacde37eed9
Brayan Yepes <abrayan.claye@gmail.com>
2024-08-02 08:17:53 -0500

    feat: add album header section

---
+++ src/components/organisms/AlbumHeader/AlbumHeader.tsx
@@ -0,0 +1,22 @@
+import { AlbumHeaderProps } from "./types"
+
+export const AlbumHeader = ({ imgUrl, sectionTitle, description, btnTitle }: AlbumHeaderProps) => {
+    const defaultDescription = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur, voluptatum corrupti hic temporibus sapiente distinctio omnis vel fugit fugiat quibusdam, suscipit aspernatur perferendis beatae, libero dicta cumque quas eos ducimus."
+    const defaultBtnTitle = "Obtener más láminas"
+    return (
+        <section className="w-full h-96 p-4 bg-gray-200 place-content-center shadow-xl">
+            <div className="grid grid-cols-2 gap-8">
+                <div className="w-full h-[420px]">
+                    <img src={imgUrl} alt="" className="w-full h-full rounded shadow-xl" />
+                </div>
+                <div className="flex flex-col justify-center space-y-8 p-4">
+                    <h1 className="text-4xl font-bold text-blue-500">{sectionTitle}</h1>
+                    <p className="text-gray-500">{description ? description : defaultDescription}</p>
+                    <div className="w-full">
+                        <button className="p-2 rounded bg-blue-500 text-white hover:bg-blue-700 transition-all">{btnTitle ? btnTitle : defaultBtnTitle}</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
---
+++ src/components/organisms/AlbumHeader/types.ts
@@ -0,0 +1,6 @@
+export interface AlbumHeaderProps {
+    imgUrl: string;
+    sectionTitle: string;
+    description?: string;
+    btnTitle?: string;
+}
